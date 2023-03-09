3c8edb1f636a061c5bc7da9e5f7ee31f45430527
liuxy1103 <54794058+liuxy1103@users.noreply.github.com>
2023-03-09 21:45:40 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -18,9 +18,9 @@ from util_metric.map_eval import eval_mito
 from util_metric.metrics import dice_coeff
 
 
-model_state_file = "/trained_model/patch_model.pth"
+model_state_file = "/trained_model/segmentation_model.pth"
 config_path = " "
-ckps = '/trained_model/seed_model.pth'
+ckps = '/trained_model/localization_model.pth'
  
 def load_nii(path):
     print(path.split("/")[-1], "loaded!")
